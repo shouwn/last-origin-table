8c94e04ceb1a5e603d55c15cdda064ae7cd6177c
shouwn <sjonghyon@gmail.com>
2019-03-28 03:39:03 +0900

    반복 탐색 효율표 구현

--- src/components/ExploreTable.vue
+++ src/components/ExploreTable.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="exploreTable" class="ui relaxed divided list">
+    <div id="repeatButton" class="ui toggle checkbox">
+      <input type="checkbox" name="public" v-model="isRepeat">
+      <label> 탐색 반복 </label>
+    </div>
     <explore-table-row
-      v-for="exploreList in exploreCombination"
+      v-for="exploreList in sortedExploreCombination"
       :key="exploreList.reduce((id, e) => { id += e.id; return id }, '')"
       :explores="exploreList"
       class="item"
@@ -23,12 +27,26 @@
         required: true
       }
     },
+    data () {
+      return {
+        isRepeat: true
+      }
+    },
     computed: {
       exploreCombination: function () {
         let exploreCombination = combination(this.explores, 4);
-        let adder = function (value, e) {
-          return value + e.resource.amount;
-        };
+        return exploreCombination;
+      },
+      sortedExploreCombination: function () {
+        let adder = null;
+
+        if (this.isRepeat) {
+          adder = this.adderAmountPerHour;
+        } else {
+          adder = this.adderAmount;
+        }
+
+        let exploreCombination = this.exploreCombination.slice();
 
         exploreCombination.sort((arr1, arr2) => {
           return arr2.reduce(adder, 0) - arr1.reduce(adder, 0);
@@ -36,16 +54,27 @@
 
         return exploreCombination.slice(0, 10);
       }
+    },
+    methods: {
+      adderAmount: function (value, e) {
+        return value + e.resource.amount;
+      },
+      adderAmountPerHour: function (value, e) {
+        return value + e.resourcePerHour.amount;
+      }
     }
   }
 </script>
 
 <style scoped>
   #exploreTable {
+    text-align: left;
     width: 50vw;
     margin-right: auto;
     margin-left: auto;
     margin-top: 6em;
   }
-
+  #repeatButton {
+    margin-bottom: 1em;
+  }
 </style>
